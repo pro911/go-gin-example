bd56b23c6b219caa5d5dccd48d1001aabe5f7273
pro911 <pro911@qq.com>
2022-02-25 20:43:21 +0800

    init

--- go.mod
+++ go.mod
@@ -2,11 +2,44 @@ module github.com/pro911/go-gin-example
 
 go 1.17
 
-
 replace (
-		github.com/pro911/go-gin-example/pkg/setting =>  ./go-application/go-gin-example/pkg/setting
-		github.com/pro911/go-gin-example/conf    	  => ./go-application/go-gin-example/pkg/conf
-		github.com/pro911/go-gin-example/middleware  => ./go-application/go-gin-example/middleware
-		github.com/pro911/go-gin-example/models 	  => ./go-application/go-gin-example/models
-		github.com/pro911/go-gin-example/routers 	  => ./go-application/go-gin-example/routers
+	github.com/pro911/go-gin-example/conf => ./conf
+	github.com/pro911/go-gin-example/middleware => ./middleware
+	github.com/pro911/go-gin-example/models => ./models
+	github.com/pro911/go-gin-example/pkg/e => ./pkg/e
+	github.com/pro911/go-gin-example/pkg/setting => ./pkg/setting
+	github.com/pro911/go-gin-example/pkg/util => ./pkg/util
+	github.com/pro911/go-gin-example/routers => ./routers
+	github.com/pro911/go-gin-example/runtime => ./runtime
+)
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/go-ini/ini v1.66.4
+	github.com/jinzhu/gorm v1.9.16
+	github.com/unknwon/com v1.0.1
+)
+
+require (
+	github.com/astaxie/beego v1.12.3 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ routers/api/v1/tag.go
@@ -0,0 +1,140 @@
+package v1
+
+import (
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-gonic/gin"
+	"github.com/pro911/go-gin-example/models"
+	"github.com/pro911/go-gin-example/pkg/e"
+	"github.com/pro911/go-gin-example/pkg/setting"
+	"github.com/pro911/go-gin-example/pkg/util"
+	"github.com/unknwon/com"
+	"net/http"
+)
+
+// GetTags 获取多个文章标签
+func GetTags(c *gin.Context) {
+	name := c.Query("name")
+
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+
+	if name != "" {
+		maps["name"] = name
+	}
+
+	var state int = -1
+	if arg := c.Query("state"); arg != "" { //获取get传参的state的值并判断如果是不是空 执行下面操作
+		state = com.StrTo(arg).MustInt() //将arg的值由string类型转换成int型并赋给变量state
+		maps["state"] = state            //将int变量state 的值赋值给map["state"]
+	}
+
+	code := e.SUCCESS //定义一个变量code值为e包内成 全局变量SUCCESS的值int 200
+
+	data["lists"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	data["total"] = models.GetTagTotal(maps)
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
+}
+
+// AddTag 新增文章标签
+func AddTag(c *gin.Context) {
+	name := c.Query("name")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	createdBy := c.Query("created_by")
+
+	valid := validation.Validation{}
+	valid.Required(name, "name").Message("名称不能为空")
+	valid.MaxSize(name, 100, "name").Message("名称最长为100字符")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.MaxSize(createdBy, 100, "created_by").Message("创建人最长为100字符")
+	valid.Range(state, 0, 1, "state").Message("状态只允许为0或1")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if !models.ExistTagByName(name) {
+			code = e.SUCCESS
+			models.AddTag(name, state, createdBy)
+		} else {
+			code = e.ERROR_EXIST_TAG
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
+}
+
+// EditTag 修改文章标签
+func EditTag(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+	name := c.Query("name")
+	modifiedBy := c.Query("modified_by")
+
+	valid := validation.Validation{}
+
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
+	}
+
+	valid.Required(id, "id").Message("ID不能为空")
+	valid.Required(modifiedBy, "modified_by").Message("修改人不能为空")
+	valid.MaxSize(modifiedBy, 100, "modified_by").Message("修改人最长为100字符")
+	valid.MaxSize(name, 100, "name").Message("名称最长为100字符")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		code = e.SUCCESS
+		if models.ExistTagByID(id) {
+			data := make(map[string]interface{})
+			data["modified_by"] = modifiedBy
+			if name != "" {
+				data["name"] = name
+			}
+			if state != -1 {
+				data["state"] = state
+			}
+
+			models.EditTag(id, data)
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
+}
+
+// DeleteTag 删除文章标签
+func DeleteTag(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必须大于0")
+
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		code = e.SUCCESS
+		if models.ExistTagByID(id) {
+			models.DeleteTag(id)
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
+}
